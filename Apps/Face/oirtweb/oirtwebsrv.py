689c74ff13c9e7eea76fbe24d33234566318a50b
pi-null-mezon <pi-null-mezon@yandex.ru>
2019-10-07 11:00:19 +0300

    reply save option has been added to python srv script

--- Apps/Face/oirtweb/oirtwebsrv.py
+++ Apps/Face/oirtweb/oirtwebsrv.py
@@ -1,4 +1,4 @@
-import subprocess, os, sys, uuid, json
+import subprocess, os, sys, uuid, json, requests
 from flask import Flask, request, redirect, jsonify, make_response
 from werkzeug.utils import secure_filename
 from waitress import serve
@@ -22,6 +22,14 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 # Flask's stuff
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Eve server section to optionally post events into mongo storage
+mongotoken = os.getenv('MONGO_AUTH_TOKEN') 
+eveurl          = os.getenv('EVE_URL')
+def save_response(methodname, response):
+	if eveurl and mongotoken:
+		requests.post(eveurl, json={'event': methodname, 'reply': response}, headers={'Authorization': 'Bearer ' + mongotoken})
+
 
 # http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 def allowed_file(filename):
@@ -50,6 +58,7 @@ def remember_face():
 			oirtsrvoutput = oirtsrvoutput.decode('cp1251')
 		response = make_response(oirtsrvoutput, 200)
 		response.headers['Content-Type'] = "application/json"
+		save_response('remember',json.loads(oirtsrvoutput))
 		return response													
 	return jsonify({"status": "Error", "info": "File you have try to upload seems to be bad"}), 400
 	
@@ -62,6 +71,7 @@ def delete_face():
 		oirtsrvoutput = oirtsrvoutput.decode('cp1251')	
 	response = make_response(oirtsrvoutput, 200)
 	response.headers['Content-Type'] = "application/json"
+	save_response('delete',json.loads(oirtsrvoutput))
 	return response	
 
 	
@@ -81,6 +91,7 @@ def identify_face():
 			oirtsrvoutput = oirtsrvoutput.decode('cp1251')
 		response = make_response(oirtsrvoutput, 200)
 		response.headers['Content-Type'] = "application/json"
+		save_response('identify',json.loads(oirtsrvoutput))
 		return response														
 	return jsonify({"status": "Error", "info": "File you have try to upload seems to be bad"}), 400
 	
@@ -101,6 +112,7 @@ def recognize_face():
 			oirtsrvoutput = oirtsrvoutput.decode('cp1251')
 		response = make_response(oirtsrvoutput, 200)
 		response.headers['Content-Type'] = "application/json"
+		save_response('recognize',json.loads(oirtsrvoutput))
 		return response														
 	return jsonify({"status": "Error", "info": "File you have try to upload seems to be bad"}), 400
 	
@@ -112,6 +124,7 @@ def get_labels():
 			oirtsrvoutput = oirtsrvoutput.decode('cp1251')
 	response = make_response(oirtsrvoutput, 200)
 	response.headers['Content-Type'] = "application/json"
+	save_response('labels',json.loads(oirtsrvoutput))
 	return response
 	
 	
@@ -136,6 +149,7 @@ def verify_face():
 		vfile.save(vfilepath)
 		response = make_response(subprocess.check_output(["oirtcli", "-a%s" % oirtsrvaddr, "-p%d" % oirtsrvport, "-i%s" % efilepath, "-v%s" % vfilepath, "-d", "-t5"]), 200)
 		response.headers['Content-Type'] = "application/json"
+		save_response('verify',json.loads(oirtsrvoutput))
 		return response														
 	return jsonify({"status": "Error", "info": "Files you have try to upload seems to be bad"}), 400	
 	
@@ -150,6 +164,7 @@ def set_whitelist():
 		json.dump(whitelist, outfile)
 	response = make_response(subprocess.check_output(["oirtcli", "-a%s" % oirtsrvaddr, "-p%d" % oirtsrvport, "-w%s" % filepath, "-d", "-t6"]), 200)
 	response.headers['Content-Type'] = "application/json"
+	save_response('whitelist',json.loads(oirtsrvoutput))
 	return response
 
 	
@@ -157,6 +172,7 @@ def set_whitelist():
 def drop_whitelist():
 	response = make_response(subprocess.check_output(["oirtcli", "-a%s" % oirtsrvaddr, "-p%d" % oirtsrvport, "-t8"]), 200)
 	response.headers['Content-Type'] = "application/json"
+	save_response('whitelist/drop',json.loads(oirtsrvoutput))
 	return response		
 	
 	
